c9de924d17d22c23a0b3541ac031291bcb24db34
Juan <jmnlsoto123>
2023-03-17 15:38:34 -0700

    working; update variable names

--- utilities/openAlgorithmEditor.js
+++ utilities/openAlgorithmEditor.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-function openAlgorithmEditor(randomAlgorithm, filePath, fileName, editor = 'vim', previousAttemptFilePath = null, attempts) {
+function openAlgorithmEditor(randomAlgorithm, modifiedAlgorithmFilePath, modifiedFileName, editor = 'vim', previousAttemptFilePath = null, attempts) {
   
-    if(fs.existsSync(filePath)){
-      fs.unlinkSync(filePath);
+    if(fs.existsSync(modifiedAlgorithmFilePath)){
+      fs.unlinkSync(modifiedAlgorithmFilePath);
     }
 
     // console.log(`\nWe are looking at attempts: ${attempts}`);
@@ -15,30 +15,30 @@ function openAlgorithmEditor(randomAlgorithm, filePath, fileName, editor = 'vim'
 
         modifiedAlgorithm = `/**\n * ${randomAlgorithm.description}\n**/\n\n${randomAlgorithm.func.toString().replace(/^function\s*\(\s*\)\s*\{([\s\S]*)\}$/, '$1')}`;
         // console.log(`Golden Goose Egg: ${modifiedAlgorithm}`);
-        fs.writeFileSync(filePath, modifiedAlgorithm);
+        fs.writeFileSync(modifiedAlgorithmFilePath, modifiedAlgorithm);
 
     } else if (previousAttemptFilePath && attempts > 1) {
 
         console.log(`\n the previousAttemptFilePath: ${previousAttemptFilePath}`);
-        let filepathToArray = filePath.split('/')
+        let filepathToArray = modifiedAlgorithmFilePath.split('/')
         filepathToArray.pop()
         filepathToArray.push(previousAttemptFilePath)
         let previousAttemptFilePathDir = filepathToArray.join('/')
         // console.log("working:",previousAttemptFilePathDir);
         const previousAttemptContent = fs.readFileSync(previousAttemptFilePathDir, 'utf8');
         // console.log(`Golden Goose Egg beyond previous function content: ${previousAttemptContent}`);
-        fs.writeFileSync(filePath, previousAttemptContent);
+        fs.writeFileSync(modifiedAlgorithmFilePath, previousAttemptContent);
     }
     
 
     //Opening The new text editor
     console.log("\nOpening the text editor.......");
-    const vim = spawn(editor, [filePath], {
+    const vim = spawn(editor, [modifiedAlgorithmFilePath], {
         stdio: 'inherit'
     });
 
     vim.on('exit', (code) => {
-        const modifiedAlgorithm = fs.readFileSync(filePath, 'utf8');
+        const modifiedAlgorithm = fs.readFileSync(modifiedAlgorithmFilePath, 'utf8');
         randomAlgorithm.func = new Function(modifiedAlgorithm);
         console.log("This is the new modified Algorithm:",randomAlgorithm.func);
     }); 
